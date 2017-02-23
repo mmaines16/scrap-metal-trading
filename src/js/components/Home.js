a6f2747170a6bf2f4ddec92f0273dcb8dd3c78d5
Mason Maines (SunGard-Lake Mary) <mason.maines@sungardps.com>
2017-02-23 16:03:48 -0500

    Add material table to Home page. Fix styles for medium size viewports

--- src/js/components/Home.js
+++ src/js/components/Home.js
@@ -2,6 +2,8 @@ import React from "react";
 
 import Banner from "./common/Banner";
 
+import {recycledMaterials} from "./resources/materials";
+
 export default class Home extends React.Component {
   render() {
 
@@ -13,6 +15,26 @@ export default class Home extends React.Component {
       position: "absoulute",
     }
 
+    const materials = recycledMaterials.map((material) => {
+        //console.log("Material: " + material.title + "\n\t\t Needs proof of ownership: " + material.proofOfOwnership);
+        if(material.proofOfOwnership == "true") {
+          return (
+            <tr class="danger">
+              <td>{material.title + "*"}</td>
+              <td class="mobile-hide">yes</td>
+            </tr>
+          );
+        }
+        else {
+          return (
+            <tr>
+              <td>{material.title}</td>
+              <td class="mobile-hide">no</td>
+            </tr>
+          );
+        }
+    });
+
     return (
       <div id="home">
         <Banner style={backgroundStyles} bannerImg="/img/scrap-metal-pile.jpg" imgScroll="true">
@@ -22,7 +44,37 @@ export default class Home extends React.Component {
             </div>
           </div>
         </Banner>
-      </div>
+
+        <div class="panel panel-success">
+          <div class="panel-heading">
+            <h2>Recycleable Materials</h2>
+            <p>
+              Below is a list of materials that can be recycled.
+              *All items in <span style={{color: "red"}}>RED</span> need <strong>Proof of Ownership</strong>
+            </p>
+          </div>
+
+          <div class="panel-body">
+            <div id="materialsTable" class="container">
+                <div class="table-responsive">
+                <table class="table table-table-condensed">
+                  <thead>
+                    <tr>
+                      <th>Material(s)</th>
+                      <th class="mobile-hide">Proof of Ownership?</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {materials}
+                  </tbody>
+                </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
     );
   }
 }
---
+++ src/js/components/resources/materials.js
@@ -0,0 +1,352 @@
+export const recycledMaterials =[
+  {
+    title: "A/C Complete Unit",
+    image: "",
+    proofOfOwnership: "false"
+  },
+  {
+    title: "A/C Compressor",
+    image: "",
+    proofOfOwnership: "false"
+  },
+  {
+    title: "All Utility Wire/Bus Bar (proof)",
+    image: "",
+    proofOfOwnership: "false"
+  },
+  {
+    title: "Aluminum",
+    image: "",
+    proofOfOwnership: "false"
+  },
+  {
+    title: "Aluminum Cans",
+    image: "",
+    proofOfOwnership: "false"
+  },
+  {
+    title: "Aluminum Dirty",
+    image: "",
+    proofOfOwnership: "false"
+  },
+  {
+    title: "Aluminum Extrusion",
+    image: "",
+    proofOfOwnership: "false"
+  },
+  {
+    title: "Aluminum Turnings",
+    image: "",
+    proofOfOwnership: "false"
+  },
+  {
+    title: "Auto A/C Compressor",
+    image: "",
+    proofOfOwnership: "false"
+  },
+  {
+    title: "Automatic Transmission",
+    image: "",
+    proofOfOwnership: "false"
+  },
+  {
+    title: "Automotive Insulation",
+    image: "",
+    proofOfOwnership: "false"
+  },
+  {
+    title: "B.B.",
+    image: "",
+    proofOfOwnership: "false"
+  },
+  {
+    title: "Backflow/Water Meter/Commercial Sprinklers Head",
+    image: "",
+    proofOfOwnership: "true"
+  },
+  {
+    title: "Ballast",
+    image: "",
+    proofOfOwnership: "false"
+  },
+  {
+    title: "Battery",
+    image: "",
+    proofOfOwnership: "false"
+  },
+  {
+    title: "Brass",
+    image: "",
+    proofOfOwnership: "false"
+  },
+  {
+    title: "Brass Dirty",
+    image: "",
+    proofOfOwnership: "false"
+  },
+  {
+    title: "Beer Kegs",
+    image: "",
+    proofOfOwnership: "true"
+  },
+  {
+    title: "Burnt Metallic Wire",
+    image: "",
+    proofOfOwnership: "true"
+  },
+  {
+    title: "Car Computers",
+    image: "",
+    proofOfOwnership: "false"
+  },
+  {
+    title: "Cast Clean",
+    image: "",
+    proofOfOwnership: "false"
+  },
+  {
+    title: "Cast Dirty",
+    image: "",
+    proofOfOwnership: "false"
+  },
+  {
+    title: "Cast Light",
+    image: "",
+    proofOfOwnership: "false"
+  },
+  {
+    title: "Catalytic Converters",
+    image: "",
+    proofOfOwnership: "true"
+  },
+  {
+    title: "Cellphones",
+    image: "",
+    proofOfOwnership: "false"
+  },
+  {
+    title: "Christmas Lights",
+    image: "",
+    proofOfOwnership: "false"
+  },
+  {
+    title: "Chrome Wheels",
+    image: "",
+    proofOfOwnership: "false"
+  },
+  {
+    title: "Computers",
+    image: "",
+    proofOfOwnership: "false"
+  },
+  {
+    title: "Copper 1",
+    image: "",
+    proofOfOwnership: "false"
+  },
+  {
+    title: "Copper 2",
+    image: "",
+    proofOfOwnership: "false"
+  },
+  {
+    title: "Copper Aluminum or Aluminum/Copper Coils",
+    image: "",
+    proofOfOwnership: "true"
+  },
+  {
+    title: "DVD Rom",
+    image: "",
+    proofOfOwnership: "false"
+  },
+  {
+    title: "Electric Motor",
+    image: "",
+    proofOfOwnership: "false"
+  },
+  {
+    title: "FDC Valves",
+    image: "",
+    proofOfOwnership: "true"
+  },
+  {
+    title: "Fire Hydrants",
+    image: "",
+    proofOfOwnership: "true"
+  },
+  {
+    title: "Fueling Tanks",
+    image: "",
+    proofOfOwnership: "true"
+  },
+  {
+    title: "Guard Rails",
+    image: "",
+    proofOfOwnership: "true"
+  },
+  {
+    title: "Hard Drives",
+    image: "",
+    proofOfOwnership: "false"
+  },
+  {
+    title: "Heavy Steel",
+    image: "",
+    proofOfOwnership: "false"
+  },
+  {
+    title: "Historic Cemetary & Grave Markers",
+    image: "",
+    proofOfOwnership: "true"
+  },
+  {
+    title: "Insulation",
+    image: "",
+    proofOfOwnership: "false"
+  },
+  {
+    title: "Laptops",
+    image: "",
+    proofOfOwnership: "false"
+  },
+  {
+    title: "LCD Flat TVs and Monitors",
+    image: "",
+    proofOfOwnership: "false"
+  },
+  {
+    title: "Lead Clean",
+    image: "",
+    proofOfOwnership: "false"
+  },
+  {
+    title: "Lead Dirty/Wheel Weights",
+    image: "",
+    proofOfOwnership: "false"
+  },
+  {
+    title: "Manhole Covers/Storm Drain Grates",
+    image: "",
+    proofOfOwnership: "true"
+  },
+  {
+    title: "Mother Boards",
+    image: "",
+    proofOfOwnership: "false"
+  },
+  {
+    title: "Motors AL",
+    image: "",
+    proofOfOwnership: "false"
+  },
+  {
+    title: "Newspaper Vending Machines",
+    image: "",
+    proofOfOwnership: "true"
+  },
+  {
+    title: "Power Supplies",
+    image: "",
+    proofOfOwnership: "false"
+  },
+  {
+    title: "Public Mailboxes",
+    image: "",
+    proofOfOwnership: "true"
+  },
+  {
+    title: "Radiator AL",
+    image: "",
+    proofOfOwnership: "false"
+  },
+  {
+    title: "Radiator AL Dirty",
+    image: "",
+    proofOfOwnership: "false"
+  },
+  {
+    title: "Radiator CU Dirty",
+    image: "",
+    proofOfOwnership: "false"
+  },
+  {
+    title: "Radiator CU/Brass",
+    image: "",
+    proofOfOwnership: "false"
+  },
+  {
+    title: "Radiator Mix",
+    image: "",
+    proofOfOwnership: "false"
+  },
+  {
+    title: "Radiator Mix Dirty",
+    image: "",
+    proofOfOwnership: "false"
+  },
+  {
+    title: "Railroad Scrap",
+    image: "",
+    proofOfOwnership: "true"
+  },
+  {
+    title: "Refer Aluminum",
+    image: "",
+    proofOfOwnership: "false"
+  },
+  {
+    title: "Refer Aluminum/Copper",
+    image: "",
+    proofOfOwnership: "false"
+  },
+  {
+    title: "Rims Aluminum",
+    image: "",
+    proofOfOwnership: "false"
+  },
+  {
+    title: "Shopping Carts",
+    image: "",
+    proofOfOwnership: "true"
+  },
+  {
+    title: "Stainless Dirty",
+    image: "",
+    proofOfOwnership: "false"
+  },
+  {
+    title: "Stainless Steel",
+    image: "",
+    proofOfOwnership: "false"
+  },
+  {
+    title: "Steel",
+    image: "",
+    proofOfOwnership: "false"
+  },
+  {
+    title: "Street Signs/Traffic Signs",
+    image: "",
+    proofOfOwnership: "true"
+  },
+  {
+    title: "Tires W/Aluminum Rims",
+    image: "",
+    proofOfOwnership: "false"
+  },
+  {
+    title: "Traffic Signals",
+    image: "",
+    proofOfOwnership: "true"
+  },
+  {
+    title: "Utility/Light Poles",
+    image: "",
+    proofOfOwnership: "true"
+  },
+  {
+    title: "Whole Computers",
+    image: "",
+    proofOfOwnership: "false"
+  },
+];
