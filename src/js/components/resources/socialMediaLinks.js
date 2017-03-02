d83ffdac9ad6fb53dd3f5325edc50a08b717f477
Mason Maines (SunGard-Lake Mary) <mason.maines@sungardps.com>
2017-03-02 12:24:01 -0500

    create and backup initial mongo database

--- src/js/components/resources/socialMediaLinks.js
+++ src/js/components/resources/socialMediaLinks.js
@@ -17,16 +17,16 @@ export const socialMediaLinks =[
     active: "true",
     faClass: "fa fa-twitter-square social-media-link",
   },
-  // {
-  //   platform: "youtube",
-  //   url: "https://youtube.com",
-  //   active: "true",
-  //   faClass: "fa fa-youtube-square social-media-link",
-  // },
-  // {
-  //   platform: "google+",
-  //   url: "https://plus.google.com/",
-  //   active: "true",
-  //   faClass: "fa fa-google-plus-square social-media-link",
-  // },
+  {
+    platform: "youtube",
+    url: "https://youtube.com",
+    active: "false",
+    faClass: "fa fa-youtube-square social-media-link",
+  },
+  {
+    platform: "google+",
+    url: "https://plus.google.com/",
+    active: "false",
+    faClass: "fa fa-google-plus-square social-media-link",
+  },
 ];
