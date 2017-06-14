2c0c906edc044488d3b01ac066c26abe03263d2e
Mason Maines -T (mmaines - INSIGHT GLOBAL INC at Cisco) <mmaines@cisco.com>
2017-06-14 18:49:48 -0400

    change server port from 3000 to 80

--- build/server.js
+++ build/server.js
@@ -9,5 +9,5 @@ server.get("/", function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
-server.listen(3000);
-console.log("Server is running on port 3000...");
+server.listen(80);
+console.log("Server is running on port 80...");
